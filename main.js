0ada63a08ec3ea6194b55f6714ea49068016499f
temiclint <dizclothing24@gmail.com>
2020-12-20 01:50:24 +0100

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-alert("hello jamiu jamal");
+
+const yname = prompt("Input ur name");
+
+const head1 = document.getElementById("head1");
+head1.innerText = `Hello ${yname}`;
+const alrt = alert(`Hello ${yname}`);
